afaff7c6a3f59057e712f7e924e4c13cfb858a5e
SamuelPerez27 <samuelpperez07@gmail.com>
2021-07-06 00:54:23 -0300

    Programa end

---
+++ Multithreading/Multithreading/Caja.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Multithreading
+{
+    class Caja
+    {
+
+        public string name;
+        public int numCaja;
+        public int clients;
+
+        public void MostrarCaja()
+        {
+
+          
+            for ( ; clients>=0; clients--)
+            {
+                
+                Console.WriteLine($"Bienvenido/a a la caja número {this.numCaja}" +
+                                  $" {this.name} lo esta atendiendo\n " +
+                                  $"Registramos y tomamos sus producto\n" +
+                                  $"Quedan {this.clients} clientes en la fila\n");
+          
+                Console.WriteLine($"Procesando orden en caja {this.numCaja}\n");
+
+         
+                Console.WriteLine($"Orden completada en caja {this.numCaja}!!!\n");
+                Thread.Sleep(1000);
+                
+            }
+            Console.WriteLine($"Se han atendido todos los cliente de la caja {this.numCaja}!!\n");
+        }
+
+    }
+}
--- Multithreading/Multithreading/Program.cs
+++ Multithreading/Multithreading/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Multithreading
@@ -10,6 +11,21 @@ namespace Multithreading
     {
         static void Main(string[] args)
         {
+            Caja caja1 = new Caja(){name = "Jocho", numCaja = 1, clients = 5};
+            Caja caja2 = new Caja() { name = "Naomys", numCaja = 2, clients = 5 };
+
+            Thread th1 = new Thread(new ThreadStart(caja1.MostrarCaja));
+            Thread th2 = new Thread(new ThreadStart(caja2.MostrarCaja));
+
+            th1.Start();
+            th2.Start();
+
+            th1.Join();
+            th2.Join();
+
+
+            Console.ReadKey();
         }
+       
     }
 }
